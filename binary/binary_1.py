cde0d01a1b0f37135b25b65912c5656107184321
Sean Currlin <sean.currlin@gmail.com>
2024-02-25 10:21:50 -0500

    added dynamic programming folder and solutions

--- binary/binary_1.py
+++ binary/binary_1.py
@@ -1,4 +1,5 @@
 # Sum of Two Integers
+
 def getSum(a: int, b: int) -> int:
     mask = 0xffffffff
     while b & mask:
@@ -6,6 +7,7 @@ def getSum(a: int, b: int) -> int:
     return a & mask if b > mask else a | b
 
 # Number of 1 Bits
+
 def hammingWeight(n: int) -> int:
     count = 0
     while n:
@@ -14,6 +16,7 @@ def hammingWeight(n: int) -> int:
     return count
 
 # Counting Bits
+
 def countBits(n: int) -> List[int]:
     res = [0] * (n + 1)
     for i in range(1, n + 1):
@@ -21,12 +24,14 @@ def countBits(n: int) -> List[int]:
     return res
 
 # Missing Number
+
 def missingNumber(nums: List[int]) -> int:
     expected_sum = len(nums) * (len(nums) + 1) // 2
     actual_sum = sum(nums)
     return expected_sum - actual_sum
 
 # Reverse Bits
+
 def reverseBits(n: int) -> int:
     res = 0
     for i in range(32):
