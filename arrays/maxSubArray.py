d5ecdc3945299dc27a7f6dc46e9fe019888093bd
Sean Currlin <sean.currlin@gmail.com>
2024-02-25 09:27:36 -0500

    more array solutions

---
+++ arrays/maxSubArray.py
@@ -0,0 +1,29 @@
+# Maximum Subarray
+
+def maxSubArray(self, nums: List[int]) -> int:
+
+# This line defines a function named `maxSubArray`. The function takes two parameters: `self` and `nums`. `self` is typically used in the context of class methods, but here it seems unnecessary as the function isn't part of a class. `nums` is expected to be a list of integers. The function is expected to return an integer.
+
+    max_sum = nums[0]
+
+# This line initializes a variable `max_sum` to the first element of `nums`. This variable will be used to keep track of the maximum subarray sum found so far.
+
+    current_sum = nums[0]
+
+# This line initializes a variable `current_sum` to the first element of `nums`. This variable will be used to keep track of the current subarray sum.
+
+    for i in range(1, len(nums)):
+
+# This line starts a for loop that iterates from 1 to `len(nums) - 1`. For each iteration, the current index is stored in the `i` variable.
+
+        current_sum = max(nums[i], current_sum + nums[i])
+
+# This line updates `current_sum` to the maximum of the `i`-th element of `nums` and the sum of `current_sum` and the `i`-th element of `nums`. This effectively calculates the maximum subarray sum ending at the `i`-th element.
+
+        max_sum = max(max_sum, current_sum)
+
+# This line updates `max_sum` to the maximum of `max_sum` and `current_sum`. This effectively keeps track of the maximum subarray sum found so far.
+
+    return max_sum
+
+# After the loop has finished, this line returns the maximum subarray sum. If all numbers in `nums` are negative, `max_sum` will be the maximum number in `nums`.
