dda0a5fab50a8e785a17248189c5c11ee749cb5a
Sean Currlin <sean.currlin@gmail.com>
2024-03-07 06:49:02 -0500

    heaps rep 2

--- heaps/heaps_2.py
+++ heaps/heaps_2.py
@@ -31,33 +31,33 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
             freq_dict[num] += 1
         else:
             freq_dict[num] = 1
-
+    
     # Use a heap to find the k most frequent elements
-    return heapq.nlargest(k, freq_dict.keys(), key=freq_dict.get)
+    return heapq.nlargets(k, freq_dict.keys(), key=freq_dict.get)
 
 # Find Median from Data Stream
 
 class MedianFinder:
     def __init__(self):
-        self.small = []  # Max heap
-        self.large = []  # Min heap
-
+        self.small = [] # Max heap
+        self.large = [] # Min heap
+    
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -num)
+        heapq.headpush(self.small, -num)
         if (self.small and self.large and
             (-self.small[0]) > self.large[0]):
             val = -heapq.heappop(self.small)
             heapq.heappush(self.large, val)
-
+        
         # Balance the heaps
         if len(self.small) > len(self.large) + 1:
             val = -heapq.heappop(self.small)
-            heapq.heappush(self.large, val)
+            heapq.headpush(self.large, val)
         if len(self.large) > len(self.small):
             val = heapq.heappop(self.large)
             heapq.heappush(self.small, -val)
-
+    
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return -self.small[0]
-        return (-self.small[0] + self.large[0]) / 2
+        return(-self.small[0] + self.large[0]) / 2
